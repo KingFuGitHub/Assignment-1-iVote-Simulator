ac9f44731ac5967a3ba23bb73fc6668e6d57ca7c
Long Nguyen <kingfu321@gmail.com>
2022-09-26 18:30:26 -0700

    7th commit

--- src/SimulationDriver.java
+++ src/SimulationDriver.java
@@ -1,7 +1,19 @@
 
 public class SimulationDriver{
+   private  int a;
 
     public static void main(String[] args){
         iVoteService.studentData();
+        int num;
+        System.out.println(a);
+        test();
+    }
+
+    public  void test(){
+        test1();
+    }
+
+    public static void test1(){
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -24,6 +24,8 @@ public class Student {
         return student;
     }
 
+    
+
 
     
 }
